0faf12fd8edbb239fb39563184305e2cfecb2fea
AndreaMinelli <minelliandrea94@gmail.com>
2023-01-18 16:18:50 +0100

    Creato metodo getMail

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log("ok", Vue);
+/*
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+Super Bonus
+Riusciamo a gestire e mostrare un loader mentre le mail non sono ancora tutte pronte
+*/
+
+const app = Vue.createApp({
+  name: "Mail List",
+  data() {
+    return {
+      mailList: [],
+    };
+  },
+  methods: {
+    getMail() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          this.mailList.push(response.data.response);
+        });
+    },
+  },
+  created() {
+    this.getMail();
+  },
+});
+
+app.mount("#root");
